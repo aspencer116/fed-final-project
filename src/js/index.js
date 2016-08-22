96f53fe874b40bf7d56359586145a43dfd43b84b
Andrew Spencer <andrew@heysparkbox.com>
2016-08-22 17:01:42 -0400

    style: proof section at large screen
    * styling of the about section including fixed positioning
    
    * fix: javascript changes to imporove efficiency
    
    * fix: javascript changes to imporove efficiency and fixed position background styled to desktop size
    
    * style: about section up to desktop size
    
    * style: typography sizing changes
    
    * style: add styling to the proof section up to desktop size
    
    * fix: add mixin for consistent section spacing

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,10 @@
+//Scroll speed adjustments for the svg characters
 $(window).scroll(function() {
   var scroll = $(window).scrollTop();
   var scroll_1 = scroll/8;
   var scroll_2 = scroll/3;
 	$(".parallax-one").css({
 		bottom: -80 + (scroll_1)
-    //     -ms-transform: translateY(80);
-    // -webkit-transform: translateY(80);
-    //         transform: translateY(80);
-    //transform: translateY(scroll)
 	});
   $("#lion--element__1").css({
 		bottom: 30 + (scroll_2)
