9d4c4c075b20867c9312ffb19b4acbf677b4b5da
Andrew Spencer <andrew@heysparkbox.com>
2016-08-24 16:44:13 -0400

    feat: SVG animals in about section
    * fix: changes to the hero and action section to improve load times
    
    * feat: add and style the  owl svg
    
    * style: add styles for the about section svg animals up to 100px wide
    
    * fix: replace IDs with classes and remove vendor prefixes
    
    * fix: add modernizr.js to track if inline svg is available'
    
    * fix: style about SVGs for large screens

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,54 @@
 //Scroll speed adjustments for the svg characters
 $(window).scroll(function() {
   var scroll = $(window).scrollTop();
-  var scroll_1 = scroll/8;
-  var scroll_2 = scroll/3;
-	$(".parallax-one").css({
-		bottom: -80 + (scroll_1)
+  var scroll_1 = scroll/10;
+  var scroll_2 = scroll/6;
+  var scroll_3 = scroll/3;
+
+  $("#c-lion__back").css({
+		bottom: -10 + (scroll_1)
 	});
-  $("#c-lion__element--1").css({
-		bottom: 30 + (scroll_2)
+  $(".c-lion__element--1").css({
+		bottom: 72 + (scroll_2)
   });
-  $("#c-lion__element--2").css({
-		bottom: -10 + (scroll_2)
+  $(".c-lion__element--2").css({
+		bottom: 0 + (scroll_3)
   });
-  $("#c-lion__element--3").css({
+  $(".c-lion__element--3").css({
 		bottom: -50 + (scroll_2)
   });
-  $("#c-lion__element--4").css({
-		bottom: -120 + (scroll_2)
+  $(".c-lion__element--4").css({
+		bottom: -120 + (scroll_3)
+  });
+  $("#c-owl__back").css({
+		bottom: 160 + (scroll_1)
+	});
+  $(".c-owl__element--1").css({
+		bottom: 364 + (scroll_2)
+  });
+  $(".c-owl__element--2").css({
+		bottom: -78 + (scroll_3)
+  });
+  $(".c-owl__element--3").css({
+		bottom: -18 + (scroll_2)
+  });
+  $(".c-owl__element--4").css({
+		bottom: -100 + (scroll_3)
+  });
+  $("#c-elephant__back").css({
+		bottom: 120 + (scroll_1)
+	});
+  $(".c-elephant__element--1").css({
+		bottom: -102 + (scroll_3)
+  });
+  $(".c-elephant__element--2").css({
+		bottom: -78 + (scroll_2)
+  });
+  $(".c-elephant__element--3").css({
+		bottom: 99 + (scroll_3)
+  });
+  $(".c-elephant__element--4").css({
+		bottom: 300 + (scroll_2)
   });
 });
 
